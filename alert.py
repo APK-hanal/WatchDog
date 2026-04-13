8a2ea4f87f0de17e14a7790edd2d5335dca2cf0d
APK-hanal <apilkhanal10@gmail.com>
2026-04-13 10:46:19 +0545

    Added a cooldown feature

--- alert.py
+++ alert.py
@@ -5,6 +5,7 @@ import os
 from datetime import datetime
 from dotenv import load_dotenv
 import pygame
+import time
 
 load_dotenv()
 RTSP_URL = os.getenv("RTSP_URL")
@@ -16,11 +17,14 @@ client = discord.Client(intents=intents)
 
 # If there is motion
 async def send_alert(image_path):
-    channel = client.get_channel(CHANNEL_ID)
-    await channel.send("Something movedddd", file=discord.File(image_path))
-    os.remove(image_path)
+    try:
+        channel = client.get_channel(CHANNEL_ID)
+        await channel.send("Something movedddd", file=discord.File(image_path))
+        os.remove(image_path)
 
 # Motion Dectection
+last_alert = 0
+cooldown = 300
 def detect_motion():
     cap = cv2.VideoCapture(RTSP_URL)
     prev_frame = None
@@ -38,13 +42,17 @@ def detect_motion():
         thresh = cv2.threshold(diff, 25, 255, cv2.THRESH_BINARY)[1]
         motion_score = thresh.sum()
         if motion_score > 100000:
-            timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-            image_path = f"alert_{timestamp}.jpg"
-            cv2.imwrite(image_path, frame)
-            asyncio.run_coroutine_threadsafe(
-                send_alert(image_path),
-                client.loop
-            )
+            global last_alert
+            current_time = time.time()
+            if current_time - last_alert > cooldown:
+                timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+                image_path = f"alert_{timestamp}.jpg"
+                cv2.imwrite(image_path, frame)
+                asyncio.run_coroutine_threadsafe(
+                    send_alert(image_path),
+                    client.loop
+                )
+                last_alert = current_time
         prev_frame = gray
 
 #what happens when the bot loads
